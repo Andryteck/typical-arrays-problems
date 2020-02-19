1cd6eaf2bbe8a3f26c86aafdc2234d3fec93abcb
Andryteck <772316andrew@gmail.com>
2020-02-19 22:18:11 +0300

    feat: func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length > 0 ?
+        array.reduce((min, cur) => {
+            if (cur < min) {
+                min = cur;
+            }
+
+            return min;
+        }, array[0]) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0
+    return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0
+    return (
+        array.reduce((previous, current) => (current += previous), 0) /
+        array.length
 }
